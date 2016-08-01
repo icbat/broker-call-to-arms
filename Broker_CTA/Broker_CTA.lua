a08cfc2622ab19d25dd204cf5ec7e6230e4b601d
Evan Cobb <cobb.evan@gmail.com>
2016-08-01 00:39:34 -0500

    shows the number of dungeons you could potentially queue for. Progress!

--- Broker_CTA/Broker_CTA.lua
+++ Broker_CTA/Broker_CTA.lua
@@ -2,6 +2,38 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 
 local dataobj = ldb:NewDataObject("Call To Arms", {type = "data source", text = "Static Text Here"})
 
+local broker_cta = {}
+
+function broker_cta.listRandomDungeons()
+   local result = {}
+   for i = 1, GetNumRandomDungeons() do
+      local id, name = GetLFGRandomDungeonInfo(i)
+      result[i] = {["id"] = id, ["name"] = name}
+   end
+   return result
+end
+
+function  broker_cta.filterToEligible(dungeons)
+    local result = {}
+    local count = 1
+    for i=1,#dungeons do
+        local eligible, needsTank, needsHealer, needsDamage = GetLFGRoleShortageRewards(dungeons[i]["id"], 1)
+        if eligible then
+            result[count] = {
+                ["needsTank"] = needsTank,
+                ["needsHealer"] = needsHealer,
+                ["needsDamage"] = needsDamage,
+                ["name"] = dungeons[i]["name"],
+                ["id"] = dungeons[i]["id"]
+            }
+
+            count = count + 1
+        end
+    end
+    return result
+end
+
+
 local f = CreateFrame("frame")
 local UPDATEPERIOD, elapsed = 0.5, 0
 f:SetScript("OnUpdate", function(self, elap)
@@ -9,10 +41,10 @@ f:SetScript("OnUpdate", function(self, elap)
 	if elapsed < UPDATEPERIOD then return end
     elapsed = 0
 
-    local text = ""
-    for i = 1, GetNumRandomDungeons() do
-      local id, name = GetLFGRandomDungeonInfo(i)
-      text = text .. id .. ": " .. name .. "\n"
+    local dungeons = broker_cta.listRandomDungeons()
+    local filtered = broker_cta.filterToEligible(dungeons)
+    if filtered ~= nil then
+        dataobj.text = #filtered
     end
-	dataobj.text = text
+
 end)
