03ddabe81d99bc961b24cf52117a9476e6434742
Evan Cobb <cobb.evan@gmail.com>
2016-07-31 23:45:05 -0500

    shows the list of all possible LFG random dungeon queues

--- Broker_CTA/Broker_CTA.lua
+++ Broker_CTA/Broker_CTA.lua
@@ -1,3 +1,18 @@
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 
-ldb:NewDataObject("Call To Arms", {type = "data source", text = "Static Text Here"})
+local dataobj = ldb:NewDataObject("Call To Arms", {type = "data source", text = "Static Text Here"})
+
+local f = CreateFrame("frame")
+local UPDATEPERIOD, elapsed = 0.5, 0
+f:SetScript("OnUpdate", function(self, elap)
+	elapsed = elapsed + elap
+	if elapsed < UPDATEPERIOD then return end
+    elapsed = 0
+
+    local text = ""
+    for i = 1, GetNumRandomDungeons() do
+      local id, name = GetLFGRandomDungeonInfo(i)
+      text = text .. id .. ": " .. name .. "\n"
+    end
+	dataobj.text = text
+end)
