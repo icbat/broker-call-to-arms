e462225eea581502fafee7e16128b2df60b66929
Evan Cobb <cobb.evan@gmail.com>
2016-07-31 23:43:41 -0500

    fixing deploy script to work with sub directories

--- copy-local.bat
+++ copy-local.bat
@@ -1,2 +1,2 @@
-del /Q "C:\Program Files (x86)\World of Warcraft\Interface\AddOns\Broker_CTA\"
+del /q /s "C:\Program Files (x86)\World of Warcraft\Interface\AddOns\Broker_CTA\"
 xcopy Broker_CTA "C:\Program Files (x86)\World of Warcraft\Interface\AddOns\Broker_CTA\" /e
