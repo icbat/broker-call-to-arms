9e01f15ed4ba4585b3a4e4b5deee181394e4eeb2
EvanCobb <evan.cobb@protonmail.com>
2020-08-09 16:38:21 -0500

    removes unused bat files, replaced w/ GH Actions

--- copy-local.bat
+++
@@ -1,2 +0,0 @@
-del /q /s "D:\World of Warcraft\Interface\AddOns\Broker_CTA\"
-xcopy Broker_CTA "D:\World of Warcraft\Interface\AddOns\Broker_CTA\" /e
--- generateArtifact.bat
+++
@@ -1,5 +0,0 @@
-:: clean up
-rmdir dist /s /q
-mkdir dist
-:: requires 7zip
-7za a dist/Broker_CTA Broker_CTA -tzip
