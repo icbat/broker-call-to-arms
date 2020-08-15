9db7e5e9b1d374d31a7c52f8ffe22484a89b33fd
EvanCobb <evan.cobb@protonmail.com>
2020-08-14 19:51:58 -0500

    splits script out into manageable files

---
+++ Broker.lua
@@ -0,0 +1,68 @@
+local LibQTip = LibStub('LibQTip-1.0')
+
+local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
+
+local addonName = "Call To Arms"
+local dataobj = ldb:NewDataObject(addonName, {
+    type = "data source",
+    text = "Broker: Call to Arms"
+})
+
+local function OnRelease(self)
+    LibQTip:Release(self.tooltip)
+    self.tooltip = nil
+end
+
+local function anchor_OnEnter(self)
+    if self.tooltip then
+        LibQTip:Release(self.tooltip)
+        self.tooltip = nil
+    end
+
+    -- Acquire a tooltip with 3 columns, respectively aligned to left, center and right
+    local tooltip = LibQTip:Acquire("FooBarTooltip", 3, "LEFT", "CENTER", "RIGHT")
+    self.tooltip = tooltip
+    tooltip.OnRelease = OnRelease
+    tooltip.OnLeave = OnLeave
+    tooltip:SetAutoHideDelay(.1, self)
+
+    broker_cta_display.build_tooltip(tooltip)
+
+    -- Use smart anchoring code to anchor the tooltip to our frame
+    tooltip:SmartAnchorTo(self)
+
+    -- Show it, et voilà !
+    tooltip:Show()
+end
+
+
+-- tooltip/broker object settings
+function dataobj:OnEnter()
+    anchor_OnEnter(self)
+end
+
+function dataobj:OnLeave()
+	-- Nothing to do. Needs to be defined for some display addons apparently
+end
+
+function dataobj:OnClick()
+    ToggleLFDParentFrame()
+end
+
+function set_label(self)
+    dataobj.text =  broker_cta_display.build_label()
+end
+
+-- invisible frame for updating/hooking events
+local f = CreateFrame("frame")
+local UPDATEPERIOD = 5
+local elapsed = 0
+f:SetScript("OnUpdate", function(self, elap)
+    elapsed = elapsed + elap
+	if elapsed < UPDATEPERIOD then return end
+    elapsed = 0
+    set_label(self)
+end)
+
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", set_label)
---
+++ Display.lua
@@ -0,0 +1,100 @@
+broker_cta_display = {}
+
+local function concatTables(t1,t2)
+    for i=1,#t2 do
+        t1[#t1+1] = t2[i]
+    end
+    return t1
+end
+
+local function coloredText(text, color)
+    return "\124c" .. color .. text .. "\124r"
+end
+
+local function wrapRoles(tank, healer, damage)
+    return {tank, healer, damage}
+end
+
+local roleNames = {"Tank", "Heal", "Damage"}
+
+local roleColors = {"003498db", "0000f269", "00e74c3c"}
+
+local function displayRoles(roles)
+    local text = ""
+    for i = 1, #roles do
+        if roles[i] then
+            text = text .. coloredText(roleNames[i], roleColors[i]) .. " "
+        end
+    end
+
+    if text == "" then
+        return "None, select some in LFG window"
+    end
+
+    return text
+end
+
+
+local function displayList(self, instanceList)
+    if instanceList == nil or #instanceList == 0 then
+        self:AddLine("No reward satchels found")
+        return
+    end
+
+    for i=1,#instanceList do
+        local text = instanceList[i]
+        self:AddLine(text)
+    end
+end
+
+function broker_cta_display.build_tooltip(self)
+    self:AddHeader("Call To Arms")
+    self:AddLine()
+
+    self:AddLine("Selected Roles")
+    local leader, tank, healer, damage = GetLFGRoles()
+    local roles = wrapRoles(tank, healer, damage)
+    self:AddLine(displayRoles(roles))
+    self:AddLine()
+    self:AddSeparator()
+
+    local tank, healer, dps = broker_cta.filter(concatTables(broker_cta.listDungeons(), broker_cta.listRaids()))
+    local canBeTank, canBeHealer, canBeDPS = UnitGetAvailableRoles("player")
+
+    if canBeTank then
+        self:AddLine(coloredText(roleNames[1], roleColors[1]))
+        displayList(self, tank)
+        self:AddLine()
+    end
+
+    if canBeHealer then
+        self:AddLine(coloredText(roleNames[2], roleColors[2]))
+        displayList(self, healer)
+        self:AddLine()
+    end
+
+    if canBeDPS then
+        self:AddLine(coloredText(roleNames[3], roleColors[3]))
+        displayList(self, dps)
+        self:AddLine()
+    end
+end
+
+function broker_cta_display.build_label()
+    local tank, healer, dps = broker_cta.filter(concatTables(broker_cta.listDungeons(), broker_cta.listRaids()))
+    local canBeTank, canBeHealer, canBeDPS = UnitGetAvailableRoles("player")
+    local displayText = ""
+    if canBeTank then
+        displayText = displayText .. coloredText(roleNames[1] .. " " .. #tank .. " ", roleColors[1])
+    end
+    if canBeHealer then
+        displayText = displayText .. coloredText(roleNames[2] .. " " .. #healer .. " ", roleColors[2])
+    end
+    if canBeDPS then
+        displayText = displayText .. coloredText(roleNames[3] .. " " .. #dps .. " ", roleColors[3])
+    end
+    return displayText
+end
+
+
+
